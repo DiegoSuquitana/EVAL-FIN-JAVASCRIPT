f1a84422cd4b798db5c1c03b7de489eb58cfee7a
DiegoSuquitana <35553620+DiegoSuquitana@users.noreply.github.com>
2020-05-18 20:22:10 -0500

    Update app.js
    Eventos click y click style.

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ function validaSigno(){
 
 var calculadora = {
     init: function(){
-        document.onclick = this.eventoTamano;
+        
         //variables
         var display = document.getElementById('display');
         var suma = document.getElementById('mas');
@@ -111,91 +111,71 @@ var calculadora = {
         var cero = document.getElementById('0');
 
         //eventos de click 
-        uno.onmousedown = function(){uno.style.transform = "scale(95%)"}            
-        uno.onmouseup = function(){uno.style.transform = "scale(100%)"}
-        dos.onmousedown = function(){dos.style.transform = "scale(95%)"}            
-        dos.onmouseup = function(){dos.style.transform = "scale(100%)"}
+        var tecla = document.querySelectorAll('.tecla');
+        for(var i=0; i< tecla.length ; i++){
+            tecla[i].addEventListener('mousedown',function(){this.style.transform="scale(95%)"})
+        }
+        for(var i=0 ; i < tecla.length ; i++){
+            tecla[i].addEventListener('mouseup',function(){this.style.transform="scale(100%)"})
+        }
+        
         //eventos
         uno.onclick = function(e){  
-            //uno.onmousedown = function(){uno.style.transform = "scale(95%)"} 
-            //validacion de caracteres en el disp 
-           // uno.addEventListener("click",function(){
+            //validacion de caracteres en el display 
                 if(display.textContent.length < 8){
                     validaIngreso(1);        
                 }
-            //})
-            //uno.onmouseup = function(){uno.style.transform = "scale(100%)"}
         }
         
         dos.onclick = function(e){
-            //dos.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(2);        
                 }
-            //})
         }
         tres.onclick = function(e){
-            //tres.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(3);        
                 }
-            //})
         }
         cuatro.onclick = function(e){
-            //cuatro.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(4);        
                 }
-            //})
         }
         cinco.onclick = function(e){
-            //cinco.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(5);        
                 }
-            //})
         }
         seis.onclick = function(e){
-            //seis.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(6);        
                 }
-            //})
         }
         siete.onclick = function(e){
-            //siete.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(7);        
                 }
-            //})
         }
         ocho.onclick = function(e){
-            //ocho.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(8);        
                 }
-            //})
         }
         nueve.onclick = function(e){
-            //nueve.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(9);        
                 }
-            //})
         }
         cero.onclick = function(e){
-            //cero.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaIngreso(0);        
                 }
-            //})
         }
         punto.onclick = function(e){
-            //punto.addEventListener("click",function(){
                 if(display.textContent.length < 8){
                     validaPunto(".");       
                 }
-            //})
         }
         on.onclick = function(e){
             resetear();
@@ -230,25 +210,6 @@ var calculadora = {
 
     },
 
-    //eventos click 2
-    eventoTamano: function(event){
-        event.onmousedown = function(){uno.style.transform = "scale(95%)"}            
-        event.onmouseup = function(){uno.style.transform = "scale(100%)"}
-
-        //alert("evento tamano");
-        //if (event.which in onmousedown) {
-        //    alert("presion");
-            onmousedown = function(){uno.style.transform = "scale(95%)"}; 
-            //elemento.style.transform = "95%";
-          //  document.querySelectorAll("[class^='teclado']")[0].style.fontSize = "xx-large";
-            
-        //}else if (event.onmouseup==true) {
-        //    alert("soltar");
-            elemento.style.transform = "100%"
-            //document.querySelectorAll("[class^='teclado']")[0].style.fontSize = "small";
-            
-        //}
-    },
 }
 
 calculadora.init();
